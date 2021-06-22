ae3ce9e50d5dbcfd8a1edc8725ae22afc4ef3d80
kbrzdem <kbr_ozdm@hotmail.com>
2021-06-22 12:11:21 +0300

    js toggle kullanıldı

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+function myFunction() {
+  var el = document.querySelector(".side-menu");
+  var menu = document.querySelector(".menu-click");
+  menu.classList.toggle("change");
+  el.classList.toggle("d-none");
+}
